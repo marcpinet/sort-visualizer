8d5b37c3f9aa501d1cfe309a6801a169f97b9d87
Marc <marc.pinet.06@gmail.com>
2022-03-10 21:04:30 -0500

    Added Quick Sort

---
+++ algorithms/quick_sort.py
@@ -0,0 +1,35 @@
+import time
+from itertools import chain
+
+
+def sort(array: list[int]):
+    """Quick Sort"""
+    start_time = time.time()
+
+    # A quick sort function that also yields the steps (without using any return statement, only yield)
+    def _quick_sort(array: list[int], left: int = None, right: int = None):
+        if left is None:
+            left = 0
+        if right is None:
+            right = len(array) - 1
+
+        if left < right:
+            pivot = array[right]
+            pivot_index = left
+
+            for i in range(left, right):
+                if array[i] <= pivot:
+                    array[i], array[pivot_index] = array[pivot_index], array[i]
+                    yield array, [array[i], array[pivot_index]], time.time() - start_time
+                    pivot_index += 1
+
+            array[pivot_index], array[right] = array[right], array[pivot_index]
+            yield array, [array[pivot_index], array[right]], time.time() - start_time
+
+            yield from _quick_sort(array, left, pivot_index - 1)
+            yield from _quick_sort(array, pivot_index + 1, right)
+
+    yield from _quick_sort(array)
+
+    total_time = time.time()
+    yield array, [-1], total_time - start_time
