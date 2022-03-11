671dbe5b37920185d1931daa8f7a861802e26367
Marc <marc.pinet.06@gmail.com>
2022-03-10 21:04:21 -0500

    Added Heap Sort

---
+++ algorithms/heap_sort.py
@@ -0,0 +1,28 @@
+import time
+
+
+def sort(array: list[int]):
+    """Heap Sort"""
+    start_time = time.time()
+
+    def heapify(array, n, i):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+
+        if l < n and array[i] < array[l]:
+            largest = l
+        if r < n and array[largest] < array[r]:
+            largest = r
+        if largest != i:
+            array[i], array[largest] = array[largest], array[i]
+            yield from heapify(array, n, largest)
+        
+        yield array, [largest], time.time() - start_time
+
+    n = len(array)
+    for i in range(n, -1, -1):
+        yield from heapify(array, n, i)
+    for i in range(n - 1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        yield from heapify(array, i, 0)
